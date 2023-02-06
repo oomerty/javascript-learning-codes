f8ba9d91218e2e2465ca189d6c670abd94ffd665
Ömer Mert ERYİĞİT <59279876+oomerty@users.noreply.github.com>
2023-02-06 17:13:30 +0300

    second commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,10 @@
+let country = "Turkiye";
+let continent = "Europe";
+let population = 80;
+
+console.log(country + ", " + continent + ", " + population);
+
+let isIsland = true; //boolean
+let language; //undefined
+
+console.log(isIsland + ", " + language + ", " + continent + ", " + population);
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,9 @@
+let js = "amazing";
+
+console.log(10+15*3-4);
+
+let firstName = "Jonas";
+let statusName = true;
+console.log(firstName)
+console.log(typeof firstName); //string
+console.log(typeof statusName); //boolean
