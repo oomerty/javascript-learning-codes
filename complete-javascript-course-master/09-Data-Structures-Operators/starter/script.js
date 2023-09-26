46e9742dcb947249b5b36c012f8f9db51deabd2e
ByTheEfsane <59279876+oomerty@users.noreply.github.com>
2023-09-26 22:49:39 +0300

    22:49

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
-
 // Data needed for first part of the section
+const weekdays = ["mon", "tue", "wed", "fri", "sat", "sun"];
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -10,21 +11,25 @@ const restaurant = {
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
   
   openingHours: {
-    thu: {
+    [weekdays[2]]: {
       open: 12,
       close: 22,
     },
-    fri: {
+    [weekdays[4]]: {
       open: 11,
       close: 23,
     },
-    sat: {
+    [weekdays[5]]: {
       open: 0, // Open 24 hours
       close: 24,
     },
   },
 
-  order: function(starterIndex, mainIndex) {
+  /* Enhanced Object Literals */
+  // order: function(starterIndex, mainIndex) {
+  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // },
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
@@ -37,9 +42,64 @@ const restaurant = {
   }
 };
 
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+if (restaurant.openingHours.sat) console.log(restaurant.openingHours.sat.open);
+
+
+/* Looping Arrays - for-of loop */
+/*const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for(const item of menu) console.log(item);
+for(const [i, el] of menu.entries()) {
+  console.log(`${i+1}: ${el}`);
+}
+
+// console.log([...menu.entries()]);*/
+
+/* Logical Assigment Operators */
+/*const rest1 = {
+  name: "Capri",
+  numGuest: 22,
+};
+
+const rest2 = {
+  name: "La Piazza",
+  owner: "Giovanni Rossi",
+};
+
+rest1.numGuest = rest1.numGuest || 10;
+rest2.numGuest = rest2.numGuest || 10;
+
+rest1.numGuest ||= 10;
+rest2.numGuest ||= 10;
+
+rest1.numGuest ??= 10;
+rest2.numGuest ??= 10;
+console.log(rest1.numGuest, rest2.numGuest);
+
+rest1.owner &&= "<ANONYMUS>";
+rest2.owner &&= "<ANONYMUS>";
+console.log(rest1.owner, rest2.owner);*/
+
+/* The Nullish Coalescing Operator (??) */
+/*restaurant.guestNmbr = 0;
+const guest = restaurant.guestNmbr || 10;
+const guestCorrect = restaurant.guestNmbr ?? 10;
+console.log(guest, guestCorrect);*/
+
+/* Short Circuiting (|| and &&) */
+/*console.log(0 || "Ömer");
+console.log("Ömer" || "Mert");
+console.log(null || undefined);
+console.log(null || undefined || "" || "Ömer" || "Mert" || 0);
+
+console.log(0 && "Ömer");
+console.log(7 && "Ömer");
+console.log(7 && "Ömer" && null && "Mert");*/
+
 /* Rest Pattern and Parameters */
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
+/*const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);*/
 
 /* Spread Operator - good for copying and combining arrays */
 /*const arr = [3, 4, 5];
