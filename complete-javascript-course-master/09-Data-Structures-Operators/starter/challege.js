8e28c10b44577529b60cef0027b530f1e6a52b38
ByTheEfsane <59279876+oomerty@users.noreply.github.com>
2023-09-30 23:47:25 +0300

    23:47

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/challege.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/challege.js
@@ -1,34 +1,33 @@
 'use strict';
 
 /*
-We're building a football betting app (soccer for my American friends �)!
-Suppose we get data from a web service about a certain game ('game' variable on
-next page). In this challenge we're gonna work with that data.
+Let's continue with our football betting app! Keep using the 'game' variable from
+before.
 
 Your tasks:
-1. Create one player array for each team (variables 'players1' and
-'players2')
-2. The first player in any player array is the goalkeeper and the others are field
-players. For Bayern Munich (team 1) create one variable ('gk') with the
-goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
-field players
-3. Create an array 'allPlayers' containing all players of both teams (22
-players)
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
-new array ('players1Final') containing all the original team1 players plus
-'Thiago', 'Coutinho' and 'Perisic'
-5. Based on the game.odds object, create one variable for each odd (called
-'team1', 'draw' and 'team2')
-6. Write a function ('printGoals') that receives an arbitrary number of player
-names (not an array) and prints each of them to the console, along with the
-number of goals that were scored in total (number of player names passed in)
-7. The team with the lower odd is more likely to win. Print to the console which
-team is more likely to win, without using an if/else statement or the ternary
-operator.
+1. Loop over the game.scored array and print each player name to the console,
+along with the goal number (Example: "Goal 1: Lewandowski")
 
-Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
-Then, call the function again with players from game.scored
- */
+2. Use a loop to calculate the average odd and log it to the console (We already
+studied how to calculate averages, you can go check if you don't remember)
+
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them
+(except for "draw"). Hint: Note how the odds and the game objects have the
+same property names
+
+4. Bonus: Create an object called 'scorers' which contains the names of the
+players who scored as properties, and the number of goals as the value. In this
+game, it will look like this:
+{
+ Gnarby: 1,
+ Hummels: 1,
+ Lewandowski: 2
+}
+*/
 
 const game = {
     team1: 'Bayern Munich',
@@ -117,3 +116,31 @@ printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
 console.log(team1 < team2 && "Team 1 wins" || "Team 2 wins");*/
 
 // Challenge 2
+/*// 1
+for (let [goalNmbr, scoredBy] of Object.entries(game.scored)) {
+    console.log(`Goal ${Number(goalNmbr)+1}: ${scoredBy}`);
+}
+
+// 2
+let oddAvg = 0;
+for (let [teamName, oddRatio] of Object.entries(game.odds)) {
+    oddAvg += oddRatio;
+}
+oddAvg = oddAvg / Object.entries(game.odds).length
+console.log(oddAvg.toFixed(2));
+
+// 3
+for (let [team, oddRatio] of Object.entries(game.odds)) {
+    console.log(`Odd of ${game[team] ? `victory ` + game[team] : `draw`}: ${oddRatio}`);
+}
+
+// 4
+let scorers = {
+}
+for (let [goalNmbr, scoredBy] of Object.entries(game.scored)) {
+    let playerGoal = 1;
+    scorers[scoredBy] = scorers[scoredBy] ? playerGoal + 1 : 1;
+}
+console.log(scorers);*/
+
+// Challenge 3
--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -3,6 +3,21 @@
 // Data needed for first part of the section
 const weekdays = ["mon", "tue", "wed", "fri", "sat", "sun"];
 
+const openingHours= {
+  [weekdays[2]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -42,9 +57,35 @@ const restaurant = {
   }
 };
 
-if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
-if (restaurant.openingHours.sat) console.log(restaurant.openingHours.sat.open);
 
+
+// Looping Objects: Object Keys, Values, Entires
+/*const properties = Object.keys(openingHours);
+
+//Property Keys
+let openStr = `We are open on ${properties.length} days: `;
+for (const days of Object.keys(openingHours)) {
+  openStr += `${days}, `;
+}
+console.log(openStr);
+
+//Property Values
+const values = Object.values(openingHours);
+console.log(values);
+
+//Property Entries = Key + Value
+const entries = Object.entries(openingHours);
+console.log(entries);
+for (const [day, {open, close}] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}.`);
+}
+*/
+
+// Optional Chaining ?.
+/*if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+if (restaurant.openingHours.sat) console.log(restaurant.openingHours.sat.open);
+console.log(restaurant.openingHours.sat?.open);*/
 
 /* Looping Arrays - for-of loop */
 /*const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
